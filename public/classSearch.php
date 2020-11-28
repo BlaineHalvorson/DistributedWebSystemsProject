99a0c989bef061ef4b6f4ada2d66971cb48991b6
BlaineHalvorson <blainehalvorson29@gmail.com>
2020-11-28 16:46:10 -0500

    student can log in and see a list of courses

--- public/classSearch.php
+++ public/classSearch.php
@@ -1,39 +1,69 @@
-<?php
+<?php require "templates/header.php";
 
 //used for debugging
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+?>
 
-if ((isset($_GET['StudentID']))&&(isset($_GET['ClassID']))) {
-  try {
-    require_once "config.php";
-    require_once "common.php";
+<h2>Course Search <?php 
+function showStudentName($name){
+  echo("<br> <h3>Logged in as " . $name . "</h3>");
+} ?></h2>
 
-    $connection = new PDO($dsn, $username, $password, $options);
+<table>
+  <tr>
+    <th>Computer Science</th>
+    <th>Biology</th>
+    <th>Nursing</th>
+  </tr>
+  <tr>
+  <td>1</td>
+  <td>2</td>
+  <td>3</td>
+  </tr>
+</table>
 
-    $sql = "SELECT classes.ClassTitle, classes.MeetingTimes, classes.MeetingDays, concat(Instructor.FirstName, ' ', Instructor.LastName) as 'Instructor', Department.DepartmentName
-    FROM classes inner join instructor on Classes.Instructor = Instructor.InstructorID
-    JOIN Department on Instructor.DepartmentID = Department.DepartmentID
-    where Classes.Department = :DepartmentID";
+<!-- Gets the student id for log in -->
+<form method="post">
+    <label for="StudentID">Student ID</label>
+    <input type="text" id="StudentID" name="StudentID">
+    <label for="DepartmentID">Department ID</label>
+    <input type="text" id="DepartmentID" name="DepartmentID">
+    <input type="submit" name="submit" value="View Results">
+</form>
 
+
+<?php
+//Gets the student name to show once logged in
+if (isset($_POST["StudentID"])) {
+  try{
+    require "config.php";
+    require "common.php";
+    
+    $connection2 = new PDO($dsn, $username, $password, $options);
+    
+    $sqlGetName = "SELECT FirstName FROM Student WHERE StudentID = :StudentID";
+    
+    $StudentID = $_POST['StudentID'];
     $DepartmentID = $_POST['DepartmentID'];
-
-    $statement = $connection->prepare($sql);
-    $statement->bindParam(':DepartmentID', $DepartmentID, PDO::PARAM_STR);
-    $statement->execute();
-
-    $result = $statement->fetchAll();
-  } catch(PDOException $error) {
-    echo $sql . "<br>" . $error->getMessage();
+    
+    $Statement2 = $connection2->prepare($sqlGetName);
+    $Statement2->bindParam(":StudentID", $StudentID, PDO::PARAM_STR);
+    $Statement2->execute();
+    $name = $Statement2->fetch();
+    
+    showStudentName($name["FirstName"]);
+    getClasses($DepartmentID, $dsn, $username, $password, $options);
+  }catch(PDOException $err) {
+    echo $sqlGetName . "<br>" . $err->getMesage();
   }
 }
-require "templates/header.php";
 
-if ((isset($_GET['StudentID']))&&(isset($_GET['DepartmentID']))) {
+// TODO: Fills and shows the table of available courses in the selected department
+function showClasses($result, $statement) {
   if ($result && $statement->rowCount() > 0) { ?>
     <h2>Results</h2>
-
     <table>
       <thead>
         <tr>
@@ -57,64 +87,32 @@ if ((isset($_GET['StudentID']))&&(isset($_GET['DepartmentID']))) {
     </tbody>
   </table>
   <?php } else { ?>
-    > No results found for <?php echo escape($_POST['DepartmentID']); ?>.
+    No results found for <?php echo escape($_POST['DepartmentID']); ?>.
   <?php }
 } 
-?>
 
-<h2>Course Search <?php 
-function showStudentName($name){
-  echo("<br> <h3>Logged in as " . $name . "<h3>");
-} ?></h2>
+function getClasses($DepartmentID, $dsn, $username, $password, $options) {
+  try {
+    $connection = new PDO($dsn, $username, $password, $options);
 
-<form method="post">
-    <label for="StudentID">Enter your Student ID</label>
-    <input type="text" id="StudentID" name="StudentID">
-    <input type="submit" name="submit" value="Log In">
-</form>
+    $sql = "SELECT classes.ClassID, classes.ClassTitle, classes.MeetingTimes, classes.MeetingDays, concat(Instructor.FirstName, ' ', Instructor.LastName) as 'Instructor', Department.DepartmentName
+    FROM classes inner join instructor on Classes.Instructor = Instructor.InstructorID
+    JOIN Department on Instructor.DepartmentID = Department.DepartmentID
+    where Classes.Department = :DepartmentID";
 
-<table>
-<?php 
-if (isset($_POST['submit'])) {
-  try{
-    require_once "config.php";
-    require_once "common.php";
+    $DepartmentID = $_POST['DepartmentID'];
 
-    $connection2 = new PDO($dsn, $username, $password, $options);
-  
-    $sqlGetName = "SELECT FirstName FROM Student WHERE StudentID = :StudentID";
-  
-    $StudentID = $_POST['StudentID'];
-  
-    $Statement2 = $connection2->prepare($sqlGetName);
-    $Statement2->bindParam(":StudentID", $StudentID, PDO::PARAM_STR);
-    $Statement2->execute();
-    $name = $Statement2->fetch();
+    $statement = $connection->prepare($sql);
+    $statement->bindParam(':DepartmentID', $DepartmentID, PDO::PARAM_STR);
+    $statement->execute();
 
-    showStudentName($name["FirstName"]);
-  }catch(PDOException $err) {
-    echo $sqlGetName . "<br>" . $err->getMesage();
+    $result = $statement->fetchAll();
+    showClasses($result, $statement);
+  } catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
   }
-  ?>
-
-  <tr>
-    <th>1</th>
-    <th>2</th>
-    <th>3</th>
-  </tr>
-  <tr>
-  <td>Computer Science</td>
-  <td>Biology</td>
-  <td>Nursing</td>
-  </tr>
-</table>
-
-<form method="post">
-    <label for="DepartmentID">Department ID</label>
-    <input type="text" id="DepartmentID" name="DepartmentID">
-    <input type="submit" name="submit" value="View Results">
-</form>
-<?php }?>
+}
+?>
 
 <a href="index.php">Back to home</a>
 
